5310aeae9ac4bbf4a57d392e8d2527a7e662fb81
VirtualPirate <artaza13020artaza@gmail.com>
2024-01-06 15:21:18 +0530

    Rendered first blog

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  images: {
+    formats: ["image/avif", "image/webp"],
+    dangerouslyAllowSVG: true,
+    remotePatterns: [{ hostname: "cdn.sanity.io" }],
+  },
 };
 
 module.exports = nextConfig;
